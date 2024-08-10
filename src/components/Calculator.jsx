358162eeef0079486c8a82be27d67a2fc7dc9c04
Deepramakhal <deepra313131@gmail.com>
2024-08-10 23:00:54 +0530

    UI updated

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -31,13 +31,13 @@ return (
         <input type="text" className='w-[95%] h-[20%] rounded-lg mb-auto m-4 text-right text-3xl font-bold px-2' value={value}
         onChange={(e)=>setvalue(e.target.value)} 
         />
-        <button className='w-[22%] h-[10%] rounded-2xl bg-[gold] hover:scale-110 active:bg-blue-600 p-2 active:text-white text-2xl font-bold border-2 hover:border-orange-500'
+        <button className='w-[22%] h-[10%] rounded-2xl bg-[lightgreen] hover:scale-110 active:bg-blue-600 p-2 active:text-white text-2xl font-bold border-2 hover:border-orange-500'
         value='' onClick={()=>backspace()}>C/CE</button>
-        <button className='w-[22%] h-[10%] rounded-2xl bg-[gold] hover:scale-110 active:bg-blue-600 p-2 active:text-white text-2xl font-bold border-2 hover:border-orange-500'
+        <button className='w-[22%] h-[10%] rounded-2xl bg-[lightgreen] hover:scale-110 active:bg-blue-600 p-2 active:text-white text-2xl font-bold border-2 hover:border-orange-500'
         value={value} onClick={(e)=>{setvalue(e.target.value*e.target.value)}}>x²</button>
-        <button className='w-[22%] h-[10%] rounded-2xl bg-[gold] hover:scale-110 active:bg-blue-600 p-2 active:text-white text-2xl font-bold border-2 hover:border-orange-500'
+        <button className='w-[22%] h-[10%] rounded-2xl bg-[lightgreen] hover:scale-110 active:bg-blue-600 p-2 active:text-white text-2xl font-bold border-2 hover:border-orange-500'
         value={sqrt} onClick={(e)=>{setvalue(e.target.value)}}>√</button>
-        <button className='w-[22%] h-[10%] rounded-2xl bg-[gold] hover:scale-110 active:bg-blue-600 p-2 active:text-white text-2xl font-bold border-2 hover:border-orange-500'
+        <button className='w-[22%] h-[10%] rounded-2xl bg-[lightgreen] hover:scale-110 active:bg-blue-600 p-2 active:text-white text-2xl font-bold border-2 hover:border-orange-500'
         value='/' onClick={(e)=>{setvalue(value + e.target.value)}}>/</button>
         <button className='w-[22%] h-[10%] rounded-2xl bg-[gold] hover:scale-110 active:bg-blue-600 p-2 active:text-white text-2xl font-bold border-2 hover:border-orange-500'
         value='9' onClick={(e)=>{setvalue(value + e.target.value)}}>9</button>
@@ -45,7 +45,7 @@ return (
         value='8' onClick={(e)=>{setvalue(value + e.target.value)}}>8</button>
         <button className='w-[22%] h-[10%] rounded-2xl bg-[gold] hover:scale-110 active:bg-blue-600 p-2 active:text-white text-2xl font-bold border-2 hover:border-orange-500'
         value='7' onClick={(e)=>{setvalue(value + e.target.value)}}>7</button>
-        <button className='w-[22%] h-[10%] rounded-2xl bg-[gold] hover:scale-110 active:bg-blue-600 p-2 active:text-white text-2xl font-bold border-2 hover:border-orange-500'
+        <button className='w-[22%] h-[10%] rounded-2xl bg-[lightgreen] hover:scale-110 active:bg-blue-600 p-2 active:text-white text-2xl font-bold border-2 hover:border-orange-500'
         value='*' onClick={(e)=>{setvalue(value + e.target.value)}}>*</button>
         <button className='w-[22%] h-[10%] rounded-2xl bg-[gold] hover:scale-110 active:bg-blue-600 p-2 active:text-white text-2xl font-bold border-2 hover:border-orange-500'
         value='6' onClick={(e)=>{setvalue(value + e.target.value)}}>6</button>
@@ -53,7 +53,7 @@ return (
         value='5' onClick={(e)=>{setvalue(value + e.target.value)}}>5</button>
         <button className='w-[22%] h-[10%] rounded-2xl bg-[gold] hover:scale-110 active:bg-blue-600 p-2 active:text-white text-2xl font-bold border-2 hover:border-orange-500'
         value='4' onClick={(e)=>{setvalue(value + e.target.value)}}>4</button>
-        <button className='w-[22%] h-[10%] rounded-2xl bg-[gold] hover:scale-110 active:bg-blue-600 p-2 active:text-white text-2xl font-bold border-2 hover:border-orange-500'
+        <button className='w-[22%] h-[10%] rounded-2xl bg-[lightgreen] hover:scale-110 active:bg-blue-600 p-2 active:text-white text-2xl font-bold border-2 hover:border-orange-500'
         value='-' onClick={(e)=>{setvalue(value + e.target.value)}}>-</button>
         <button className='w-[22%] h-[10%] rounded-2xl bg-[gold] hover:scale-110 active:bg-blue-600 p-2 active:text-white text-2xl font-bold border-2 hover:border-orange-500'
         value='3' onClick={(e)=>{setvalue(value + e.target.value)}}>3</button>
@@ -61,13 +61,13 @@ return (
         value='2' onClick={(e)=>{setvalue(value + e.target.value)}}>2</button>
         <button className='w-[22%] h-[10%] rounded-2xl bg-[gold] hover:scale-110 active:bg-blue-600 p-2 active:text-white text-2xl font-bold border-2 hover:border-orange-500'
         value='1' onClick={(e)=>{setvalue(value + e.target.value)}}>1</button>
-        <button className='w-[22%] h-[10%] rounded-2xl bg-[gold] hover:scale-110 active:bg-blue-600 p-2 active:text-white text-2xl font-bold border-2 hover:border-orange-500'
+        <button className='w-[22%] h-[10%] rounded-2xl bg-[lightgreen] hover:scale-110 active:bg-blue-600 p-2 active:text-white text-2xl font-bold border-2 hover:border-orange-500'
         value='+' onClick={(e)=>{setvalue(value + e.target.value)}}>+</button>
         <button className='w-[22%] h-[10%] rounded-2xl bg-[gold] hover:scale-110 active:bg-blue-600 p-2 active:text-white text-2xl font-bold border-2 hover:border-orange-500'
         value='0' onClick={(e)=>{setvalue(value + e.target.value)}}>0</button>
         <button className='w-[22%] h-[10%] rounded-2xl bg-[gold] hover:scale-110 active:bg-blue-600 p-2 active:text-white text-2xl font-bold border-2 hover:border-orange-500'
         value='.' onClick={(e)=>{setvalue(value + e.target.value)}}>.</button>
-        <button className='w-[44%] h-[10%] rounded-2xl bg-[gold] hover:scale-110 active:bg-blue-600 p-2 active:text-white text-2xl font-bold border-2 hover:border-orange-400'
+        <button className='w-[44%] h-[10%] rounded-2xl bg-[lightgreen] hover:scale-110 active:bg-blue-600 p-2 active:text-white text-2xl font-bold border-2 hover:border-orange-400'
         value='=' onClick={()=>calculate()}>=</button>
     </div>
     </div>
