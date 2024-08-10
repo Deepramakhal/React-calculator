60b0f97cbef8253b38d86fcf8209f28ddc5f7d31
Deepramakhal <deepra313131@gmail.com>
2024-08-10 23:20:35 +0530

    UI Update

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -28,7 +28,7 @@ return (
 <>
     <div className='w-screen h-screen flex justify-center items-center'>
     <div id="calculator-box" className='w-screen h-[70%] m-4 md:w-[40%] md:min-h-64 flex flex-wrap justify-around items-center p-4 bg-[#212121] rounded-lg'>
-        <input type="text" className='w-[95%] h-[20%] rounded-lg mb-auto m-4 text-right text-3xl font-bold px-2' value={value}
+        <input type="text" className='w-[95%] h-[20%] rounded-lg mb-auto m-4 text-right text-4xl font-bold px-2 bg-slate-300' value={value}
         onChange={(e)=>setvalue(e.target.value)} 
         />
         <button className='w-[22%] h-[10%] rounded-2xl bg-[lightgreen] hover:scale-110 active:bg-blue-600 p-2 active:text-white text-2xl font-bold border-2 hover:border-orange-500'
@@ -67,7 +67,7 @@ return (
         value='0' onClick={(e)=>{setvalue(value + e.target.value)}}>0</button>
         <button className='w-[22%] h-[10%] rounded-2xl bg-[gold] hover:scale-110 active:bg-blue-600 p-2 active:text-white text-2xl font-bold border-2 hover:border-orange-500'
         value='.' onClick={(e)=>{setvalue(value + e.target.value)}}>.</button>
-        <button className='w-[44%] h-[10%] rounded-2xl bg-[lightgreen] hover:scale-110 active:bg-blue-600 p-2 active:text-white text-2xl font-bold border-2 hover:border-orange-400'
+        <button className='w-[44%] h-[10%] rounded-2xl bg-[#e45f5f] hover:scale-110 active:bg-blue-600 p-2 active:text-white text-2xl font-bold border-2 hover:border-orange-400'
         value='=' onClick={()=>calculate()}>=</button>
     </div>
     </div>
